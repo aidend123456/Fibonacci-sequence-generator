507fac686ccb846e08f38a02375d7b65017ffb98
Hexagonal <aidendu18@gmail.com>
2025-07-03 20:33:46 -0400

    Add files via upload

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,17 @@
 import os
+
+def banner():
+    print("Fibonacci Sequence Generator")
+    print("--------------------------------------------------------------------------------")
+    print("This program generates the Fibonacci sequence up to a specified number of terms.")
+    print("You can also customize the first two numbers of the sequence.")
+    print("--------------------------------------------------------------------------------")
 def clear_screen():
     # Clear the console screen
     os.system('cls' if os.name == 'nt' else 'clear')
 
 clear_screen()
-
+banner()
 terms = int(input("Enter the number of terms you want: "))
 customized = input("Do you want to customize the first two numbers? (y/n): ")
 if customized == 'y':
